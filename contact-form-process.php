d32732e83785bf8b9cc5971ec6223e03afaf468a
brettruehland <110261431+brettruehland@users.noreply.github.com>
2022-12-12 11:15:23 +1000

    Update contact-form-process.php

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
+    $email_to = "psych@brettruehland.com";
+    $email_subject = "New webform submission";
 
     function problem($error)
     {
